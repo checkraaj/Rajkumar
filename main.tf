569715a3b91fd9f39e3d62bfa2221c92a4f17d2f
checkraaj <checkraaj@users.noreply.github.com>
2022-12-13 18:50:19 +0530

    First commit for

--- main.tf
+++ main.tf
@@ -18,12 +18,25 @@ provider "azurerm" {
   features {}
 }
 
-# Your code goes here
- resource "random_pet" "rg_name" {
-  prefix = "rg"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = "eastus"
-  name     = "TestRGDemo"
-} 
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
